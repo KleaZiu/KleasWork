427ec5e14644e0ecbe07d37402018cabf0bf2eea
DajanaRexhaj <135461543+DajanaRexhaj@users.noreply.github.com>
2023-09-15 12:56:01 +0200

    Update photoFunc.js

--- photo/photoFunc.js
+++ photo/photoFunc.js
@@ -16,7 +16,7 @@ function closeFullImg() {
 
 const image_array = [
 
-    '/KleasWork/photo/Askoll Elektrik/Askoll Elektrik NGS (1).jpg',
+    '/photo/Askoll Elektrik/Askoll Elektrik NGS (1).jpg',
     '/projects/KleasWork/photo/Askoll Elektrik/Askoll Elektrik NGS (2).jpg',
     '/projects/KleasWork/photo/Askoll Elektrik/Askoll Elektrik NGS (3).jpg',
     '/projects/KleasWork/photo/Askoll Elektrik/askoll presentation (1).jpg',
